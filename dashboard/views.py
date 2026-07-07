624dd7b14844cc3e4d9f4f3538a534317a69d531
TrishonP <trishonp1@gmail.com>
2026-07-06 20:01:04 -0600

    Implement TicketListView and create all_tickets.html template; remove old base.html and reorganized it to child of template folder

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ticket
+"""
+Class-based views:
 
+View         = Generic View
+ListView     = get a list of records
+DetailView   = get a single (detail) record
+CreateView   = create a new record
+DeleteView   = remove a record
+UpdateView   = modify an existing record
+LoginView    = LogIn
+
+"""
 # Create your views here.
+
+class TicketListView(ListView):
+    model = ticket
+    template_name = "dashboard/all_tickets.html"
